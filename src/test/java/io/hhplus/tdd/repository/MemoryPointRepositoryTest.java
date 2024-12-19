dc6e101f73deb3f0fbd3645fc9c5da90b7e6d2a1
sehyeogi365 <lsh27572757@gmail.com>
2024-12-19 11:51:48 +0900

    [임세혁] -중간 커밋 포인트 사용 다른 사람 포인트 사용 진행중,동시에 여러 사용자가 한 사용자에게 포인트 충전할시, 한 사용자가 다른 사용자 계정에 포인트 충전할시,10,000 포인트 단위로 충전안할시 아직 안됨

--- src/test/java/io/hhplus/tdd/repository/MemoryPointRepositoryTest.java
+++ src/test/java/io/hhplus/tdd/repository/MemoryPointRepositoryTest.java
@@ -2,6 +2,7 @@ package io.hhplus.tdd.repository;
 
 import io.hhplus.tdd.domain.Member;
 import io.hhplus.tdd.domain.Point;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -57,7 +58,6 @@ public class MemoryPointRepositoryTest {
         //then 결과가 이게 나와야 돼
         if(point.getAmount() < 0){
             System.out.println("음수로 입력하면 안됩니다.");
-
         }
     }
 
@@ -75,8 +75,10 @@ public class MemoryPointRepositoryTest {
 
         //then 결과가 이게 나와야 돼
 
-        if(point.getAmount() > 100000){
+        if(point.getAmount() > 1000000){
             System.out.println("1000000원 초과해서 충전할수 없습니다.");
+        } else {
+            System.out.println("충전이 완료 되었습니다." + point.getAmount() + "원");
         }
 
     }
@@ -157,26 +159,55 @@ public class MemoryPointRepositoryTest {
         } else {
             System.out.println("포인트 사용 됨 나머지 포인트 : " + (point.getAmount() - useAmount));
         }
-
-
     }
-    //한도 내역 초과해서 사용시 실패
+    //한도 내역 초과해서 100,000 포인트 초과 사용시 실패
     @Test
-    public void overUser_then_user_Limit(){
+    public void overUse_then_use_Limit(){
         //given 뭔가가 주어졌는데
+        Point point = new Point();
+        point.setAmount(100001);
+        int useAmount = 100000;
         //when 이거를 실행했을 떄
+        repository.use(point);
+
         //then 결과가 이게 나와야 돼
+        if(useAmount < point.getAmount()){
+            System.out.println("한도내역 초과입니다.");
+        }
 
     }
 
-    //한 사용자가 다른 사용자 포인트 사용
+    //한 사용자가 다른 사용자 포인트 사용(보류)
     @Test
     public void use_another_Person_Point(){
         //given 뭔가가 주어졌는데
+        Point point1 = new Point();
+        point1.setName("spring1");
+        point1.setAmount(10000);
+
+        Point point2 = new Point();
+        point2.setName("spring2");
+        point2.setAmount(10000);
+
+        repository.charge(point1);
+        repository.charge(point2);
+
+        int useAmount = 5000;
+
         //when 이거를 실행했을 떄
+        Point usedPoint = repository.use(point2);//스프링1이 스프링2의 포인트를 사용한다.
+
+
         //then 결과가 이게 나와야 돼
+        Assertions.assertThat(usedPoint).isNull(); // 사용 불가 시 null 반환 (가정)
 
+        // spring2의 포인트는 변경되지 않아야 함
+        Point result2 = repository.findByName("spring2").get();
+        Assertions.assertThat(result2.getAmount()).isEqualTo(10000);
+
+        // spring1의 포인트도 여전히 10000이어야 함
+        Point result1 = repository.findByName("spring1").get();
+        Assertions.assertThat(result1.getAmount()).isEqualTo(10000);
     }
-
 
 }
